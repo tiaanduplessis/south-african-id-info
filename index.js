abdfffffcf673ff27737c129b025067e65c7d74b
= <tiaanduplessis@hotmail.com>
2021-03-15 15:57:25 +0200

    fix: year parsing

--- index.js
+++ index.js
@@ -3,7 +3,17 @@
 const validSouthAfricanId = require('valid-south-african-id')
 
 function parseDate (str) {
-  return new Date(`${str.substr(0, 2)}-${str.substr(2, 2)}-${str.substr(4, 2)}`)
+  const d = new Date()
+  const divider = (new Date()).getFullYear().toString().substr(2, 2)
+  const parsedYear = Number.parseInt(str.substr(0, 2))
+  const year = parsedYear > divider ? `19${parsedYear.toString().padStart(2, '0')}` : `20${parsedYear.toString().padStart(2, '0')}`
+  const day = Number.parseInt(str.substr(4, 2))
+  const month = Number.parseInt(str.substr(2, 2)) - 1
+
+  d.setDate(day)
+  d.setMonth(month)
+  d.setFullYear(year)
+  return d
 }
 
 // https://www.westerncape.gov.za/general-publication/decoding-your-south-african-id-number-0
--- test.js
+++ test.js
@@ -3,7 +3,36 @@
 
 const southAfricanIdInfo = require('./')
 
+const ids = [
+  {
+    value: '9301215029086',
+    expectedResult: {
+      birthDate: 'Thu Jan 21 1993',
+      gender: 'male'
+    }
+  },
+  {
+    value: '0107224226186',
+    expectedResult: {
+      birthDate: 'Sun Jul 22 2001',
+      gender: 'female'
+    }
+  },
+  {
+    value: '1902264822087',
+    expectedResult: {
+      birthDate: 'Tue Feb 26 2019',
+      gender: 'female'
+    }
+  }
+]
+
 test('south-african-id-info', () => {
   expect(southAfricanIdInfo('').valid).toBeFalsy()
-  expect(southAfricanIdInfo('9301215029086').valid).toBeTruthy()
+  ids.forEach(({value, expectedResult}) => {
+    const result = southAfricanIdInfo(value)
+    expect(result.valid).toBeTruthy()
+    expect(result.birthDate.toDateString()).toBe(expectedResult.birthDate)
+    expect(result.gender).toBe(expectedResult.gender)
+  })
 })
